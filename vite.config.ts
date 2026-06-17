ddda1ab46ed2a2c8731ab2bc9b94f969b73237f5
Hakan <hakanroglu@gmail.com>
2026-06-17 01:30:03 -0400

    change out directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	base: "/misprint_onboarding_demo/",
+	build: {
+		outDir: "docs",
+	},
 });
